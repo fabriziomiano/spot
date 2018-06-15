a48133eea312ed6e92d8c35ae669f7f34bca61ca
Fabrizio Miano <fabriziomiano@gmail.com>
2018-06-15 23:31:39 +0100

    Add area calc as a trial

--- src/try_thresholds.py
+++ src/try_thresholds.py
@@ -29,35 +29,42 @@ grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
 diff = (diff * 255).astype("uint8")
 
 # basic threshold
-ret,thresh1 = cv2.threshold(diff,127,255,cv2.THRESH_BINARY)
+# ret,thresh1 = cv2.threshold(diff,127,255,cv2.THRESH_BINARY) 
 ret,thresh2 = cv2.threshold(diff,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(diff,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(diff,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(diff,127,255,cv2.THRESH_TOZERO_INV)
-thresh6 = cv2.adaptiveThreshold(diff,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY_INV,11,2)
-titles = ['Original_diff','BINARY','BINARY_INV','TRUNC','TOZERO',\
-          'TOZERO_INV','ADAPTIVE_THRESH_MEAN_C']
-images = [diff, thresh1, thresh2, thresh3, thresh4, thresh5, thresh6]
+# ret,thresh3 = cv2.threshold(diff,127,255,cv2.THRESH_TRUNC)
+# ret,thresh4 = cv2.threshold(diff,127,255,cv2.THRESH_TOZERO)
+# ret,thresh5 = cv2.threshold(diff,127,255,cv2.THRESH_TOZERO_INV)
+# thresh6 = cv2.adaptiveThreshold(diff,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+#             cv2.THRESH_BINARY_INV,11,2)
+# titles = ['Original_diff','BINARY','BINARY_INV','TRUNC','TOZERO',\
+#           'TOZERO_INV','ADAPTIVE_THRESH_MEAN_C']
+titles = ['Original image', 'BINARY_INV']
+# images = [diff, thresh1, thresh2, thresh3, thresh4, thresh5, thresh6]
+images = [diff, thresh2]
 
-for i in xrange(7):
-    plt.subplot(1,1,1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-    plt.savefig(args["output"] + "features_" + titles[i])
+# for i in xrange(7):
+for i in xrange(2):
+    # plt.subplot(1,1,1),plt.imshow(images[i],'gray')
+    # plt.title(titles[i])
+    # plt.xticks([]),plt.yticks([])
+    # plt.savefig(args["output"] + "features_" + titles[i])
 
     cnts = cv2.findContours(images[i].copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
+    if i == 1:
+        print str(len(cnts)) + " contours found" 
+    
     # loop over the contours
     for c in cnts:
         # compute the bounding box of the contour and then draw the
         # bounding box on both input images to represent where the two
         # images differ
         (x, y, w, h) = cv2.boundingRect(c)
-        #cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        area = cv2.contourArea(c)
         
-    cv2.imwrite(args["output"] + "features_" + titles[i] + ".png", imageB)
-    imageB = cv2.imread(args["second"])
+        if area > 900  : 
+            cv2.imwrite(args["output"] + "features_" + titles[i] + ".png", imageB)
+            imageB = cv2.imread(args["second"])
