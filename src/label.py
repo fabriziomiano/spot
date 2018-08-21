a7830ef8b40a4a00c74bfbfb67ff2da84256b14e
Fabrizio Miano <fabriziomiano@gmail.com>
2018-08-21 01:50:11 +0100

    Add docstrings. Fix dictionary creation. Use defaultdict

--- src/label.py
+++ src/label.py
@@ -1,6 +1,29 @@
+"""
+This script loads an SVM previously trained, 
+reads images from a given path, 
+extracts their features and classifies them. 
+It then writes a json file with the following structure:
+{
+    "img": {
+        "class 0": probability[0],
+        "class 1": probability[1]
+    },
+    ...
+    ...
+}
+
+where : 
+  - "img" is the file name of the image being labelled
+  - "class i" is the i-th class
+  - "probability[i]" is the i-th probability associated 
+  with the prediction of the i-th class.
+
+"""
+
 from settings.constants import EXTENSION, N_CLUSTERS, JSON_PATH
 from utils.misc import load_image, crop_to_half, fex, save_json
 from sklearn.externals import joblib
+from collections import defaultdict
 import argparse
 import sys
 import os
@@ -8,23 +31,25 @@ import numpy as np
 
 args = sys.argv
 parser = argparse.ArgumentParser(
-    description="""Predict labels for all the images within a given directory""")
+    description="""Predict labels for images within a given directory""")
 parser.add_argument('test_path',
                     type=str,
                     help='Specify the path of the directory containing images to label')
 parser.add_argument('svm_file',
                     type=str,
-                    help='Specify the file containing the SVM info')
+                    help='Specify the file containing the SVM')
 args = parser.parse_args()
-
 TEST_DIR = args.test_path
-img_dict = dict()
-results = dict()
+SVM_FILE = args.svm_file
+results = defaultdict(dict)
 try:
-    clf = joblib.load(args.svm_file)
-except Exception as e:
-    print "File " + args.svm_file + " not a valid file. Please retrain"
-    raise e
+    clf = joblib.load(SVM_FILE)
+    print "File " + SVM_FILE + " read succesfully"
+    print "Classifying images..."
+except IOError as e:
+    print "File " + SVM_FILE + " not a valid file. Please retrain"
+    raise
+classes = clf.classes_
 for img in os.listdir(TEST_DIR):
     if img.endswith(EXTENSION):
         img_path = os.path.join(TEST_DIR, img)
@@ -32,8 +57,8 @@ for img in os.listdir(TEST_DIR):
         # uncomment the line below if sky is always approximately in the upper half of the images
         # image = crop_to_half(image)
         test_feature = fex(image, N_CLUSTERS)
-        probability = clf.predict_proba([test_feature])
-        for i, classes in enumerate(clf.classes_):
-            img_dict[clf.classes_[i]] = round(probability[0][i], 3)
-        results[img] = img_dict
+        probabilities = clf.predict_proba([test_feature])
+        for i, c in enumerate(classes):
+            results[img][c] = round(probabilities[0][i], 3)
 save_json(JSON_PATH, results)
+print "Results saved in " + JSON_PATH
