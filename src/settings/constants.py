3c63280a4aca853ba15ac58cf144417505c9472b
Fabrizio Miano <fabriziomiano@gmail.com>
2018-08-21 01:51:01 +0100

    Nuances

--- src/settings/constants.py
+++ src/settings/constants.py
@@ -3,5 +3,5 @@ import os
 EXTENSION = '.jpg'
 DATE_FMT = "%Y-%m-%d-%H-%M-%S"
 N_CLUSTERS = 3
-SVM_FILENAME = 'trained_svm.pkl'
-JSON_PATH = os.path.join(os.getcwd(), 'results.json')
+SVM_FILENAME = 'svm.pkl'
+JSON_PATH = os.path.join(os.getcwd(), 'classification.json')
