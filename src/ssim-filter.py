4eb3d5818dce56a1363869c447baba0ffccf9b90
Fabrizio Miano <fabriziomiano@gmail.com>
2018-08-22 14:32:59 +0100

    Add ConfigParser to use config file

--- src/ssim-filter.py
+++ src/ssim-filter.py
@@ -6,27 +6,25 @@ the images that pass a certain threshold
 and tolerance to a selected output path 
 
 """
-from utils.misc import ssim_filter
+
+from utils.misc import ssim_filter, load_config
 import argparse
+import ConfigParser
 import sys
 
 args = sys.argv
 parser = argparse.ArgumentParser(
     description="""Select images solely relying on SSIM score against a reference image""")
-parser.add_argument('-i', '--inDir', type=str, metavar='',
-                    help='Specify the path of the datset to filter')
-parser.add_argument('-o', '--outDir', type=str, metavar='',
-                    help='Specify the destination path for the filtered dataset')
-parser.add_argument('-r','--refimg', type=str, metavar='',
-                    help='Specify the path of the reference image')
-parser.add_argument('-th','--threshold', type=float, metavar='',
-                    help='Specify the threshold for the SSIM test')
-parser.add_argument('-to','--tolerance', type=float, metavar='',
-                    help='Specify the tolerance in decimal (e.g. 0.05)')
+parser.add_argument('-c', '--config', type=str, metavar='', required=True,
+                    help='Specify the path of the configuration file')
 args = parser.parse_args()
-PATH_IN = args.inDir
-PATH_OUT = args.outDir
-REF_IMG_PATH = args.refimg
-THRESHOLD = args.threshold
-TOLERANCE = args.tolerance
-ssim_filter(PATH_IN, PATH_OUT, REF_IMG_PATH, THRESHOLD, TOLERANCE)
+CONFIG_PATH = args.config
+Config = load_config(CONFIG_PATH)
+if len(Config.sections()) == 0:
+    print "ERROR :: Not a valid configuration file. Exiting"
+    sys.exit(0)
+PATH_IN = Config.get('SSIM', 'PATH_IN')
+PATH_OUT = Config.get('SSIM', 'PATH_OUT')
+REF_IMG_PATH = Config.get('SSIM', 'REF_IMG_PATH')
+THRESHOLD = float(Config.get('SSIM', 'THRESHOLD'))
+ssim_filter(CONFIG_PATH, PATH_IN, PATH_OUT, REF_IMG_PATH, THRESHOLD)
