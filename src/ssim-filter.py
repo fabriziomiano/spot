eb9f04541c0418e13c28742a9ccb4676454664ae
Fabrizio Miano <fabriziomiano@gmail.com>
2018-08-22 11:09:10 +0100

    Fix parser

--- src/ssim-filter.py
+++ src/ssim-filter.py
@@ -17,7 +17,7 @@ parser.add_argument('-i', '--inDir', type=str, metavar='',
                     help='Specify the path of the datset to filter')
 parser.add_argument('-o', '--outDir', type=str, metavar='',
                     help='Specify the destination path for the filtered dataset')
-parser.add_argument('-r','--ref-img', type=str, metavar='',
+parser.add_argument('-r','--refimg', type=str, metavar='',
                     help='Specify the path of the reference image')
 parser.add_argument('-th','--threshold', type=float, metavar='',
                     help='Specify the threshold for the SSIM test')
@@ -25,8 +25,8 @@ parser.add_argument('-to','--tolerance', type=float, metavar='',
                     help='Specify the tolerance in decimal (e.g. 0.05)')
 args = parser.parse_args()
 PATH_IN = args.inDir
-PATH_OUT = args.destination_path
-REF_IMG_PATH = args.ref_img_path
+PATH_OUT = args.outDir
+REF_IMG_PATH = args.refimg
 THRESHOLD = args.threshold
 TOLERANCE = args.tolerance
 ssim_filter(PATH_IN, PATH_OUT, REF_IMG_PATH, THRESHOLD, TOLERANCE)
