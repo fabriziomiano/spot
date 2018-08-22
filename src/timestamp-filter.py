8f8a59f9a3dffc5267ea249278eb2eea9a1afeef
Fabrizio Miano <fabriziomiano@gmail.com>
2018-08-22 14:33:49 +0100

    Add ConfigParser to use config file

--- src/timestamp-filter.py
+++ src/timestamp-filter.py
@@ -1,31 +1,32 @@
 """
 This script filters a dataset of images 
 in a given path using timestamp information
-from the file names. 
+from the file names. It copies the images that
+pass the selection to a given output path. 
 Please refer to the timestamp_filter() 
 function in utils/misc.py for additional info.
-It copies the images that pass the selection
-to a given output path. 
 
 """
-from utils.misc import timestamp_filter
+
+from utils.misc import timestamp_filter, load_config
+import ConfigParser
 import argparse
 import sys
+import os
 
 args = sys.argv
 parser = argparse.ArgumentParser(
     description="""Select sunset / sunrise images solely relying on their timestamp""")
-parser.add_argument('-i', '--inDir', type=str, metavar='', required=True,
-                    help='Specify the path of the datset to filter')
-parser.add_argument('-o', '--outDir', type=str, metavar='', required=True,
-                    help='Specify the destination path for the filtered dataset')
-parser.add_argument('-tz', '--timezone', type=str, metavar='',
-                    required=True, help='Specify the timezone')
-parser.add_argument('-c', '--city', type=str, metavar='', required=True,
-                    help='Specify the city name')
+parser.add_argument('-c', '--config', type=str, metavar='', required=True,
+                    help='Specify the path of the configuration file')
 args = parser.parse_args()
-PATH_IN = args.inDir
-PATH_OUT = args.outDir
-TIMEZONE = args.timezone
-CITY_NAME = args.city
-timestamp_filter(PATH_IN, PATH_OUT, TIMEZONE, CITY_NAME)
+CONFIG_PATH = args.config
+Config = load_config(CONFIG_PATH)
+if len(Config.sections()) == 0:
+    print "ERROR :: Not a valid configuration file. Exiting"
+    sys.exit(0)
+PATH_IN = Config.get('TIMESTAMP', 'PATH_IN')
+PATH_OUT = Config.get('TIMESTAMP', 'PATH_OUT')
+TIMEZONE = Config.get('TIMESTAMP', 'TIMEZONE')
+CITY_NAME = Config.get('TIMESTAMP', 'CITY_NAME')
+timestamp_filter(CONFIG_PATH, PATH_IN, PATH_OUT, TIMEZONE, CITY_NAME)
