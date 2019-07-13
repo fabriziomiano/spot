2158aef8f6a9e95b931e2b9644d42069dfb4e1f4
Fabrizio Miano <fabriziomiano@gmail.com>
2019-07-13 17:22:56 +0200

    py3 port: config parser and printouts

--- src/timestamp-filter.py
+++ src/timestamp-filter.py
@@ -7,26 +7,24 @@ Please refer to the timestamp_filter()
 function in utils/misc.py for additional info.
 
 """
-
-from utils.misc import timestamp_filter, load_config
-import ConfigParser
+from .utils.misc import timestamp_filter, load_config
 import argparse
 import sys
-import os
 
-args = sys.argv
+
 parser = argparse.ArgumentParser(
     description="""Select sunset / sunrise images solely relying on their timestamp""")
-parser.add_argument('-c', '--config', type=str, metavar='', required=True,
-                    help='Specify the path of the configuration file')
+parser.add_argument(
+    '-c', '--config', type=str, metavar='', required=True,
+    help='Specify the path of the configuration file')
 args = parser.parse_args()
 CONFIG_PATH = args.config
-Config = load_config(CONFIG_PATH)
-if len(Config.sections()) == 0:
-    print "ERROR :: Not a valid configuration file. Exiting"
+conf = load_config(CONFIG_PATH)
+if len(conf.sections()) == 0:
+    print("ERROR :: Not a valid configuration file. Exiting")
     sys.exit(0)
-PATH_IN = Config.get('TIMESTAMP', 'PATH_IN')
-PATH_OUT = Config.get('TIMESTAMP', 'PATH_OUT')
-TIMEZONE = Config.get('TIMESTAMP', 'TIMEZONE')
-CITY_NAME = Config.get('TIMESTAMP', 'CITY_NAME')
+PATH_IN = conf.get('TIMESTAMP', 'PATH_IN')
+PATH_OUT = conf.get('TIMESTAMP', 'PATH_OUT')
+TIMEZONE = conf.get('TIMESTAMP', 'TIMEZONE')
+CITY_NAME = conf.get('TIMESTAMP', 'CITY_NAME')
 timestamp_filter(CONFIG_PATH, PATH_IN, PATH_OUT, TIMEZONE, CITY_NAME)
